21a2ca0c7e76756f196a169fab3e3e378fbed901
Shoaibalirts <shoaibali.rts@gmail.com>
2024-12-17 14:36:12 +0100

    made header and footer as seperate components and used them via include statement to add both header and footer

--- our-mission.php
+++ our-mission.php
@@ -1,34 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="./styles/simple.css" />
-  <link rel="stylesheet" href="./styles/custom.css" />
-  <title>Culinary Cove &bull; Our mission</title>
-</head>
-
-<body>
-  <?php
-  $title = 'Culinary Cove';
-  $src = 'ingredients.php';
-  ?>
-  <header
-    class="header-with-background"
-    style="background-image: url('images/pexels-julia-volk-5273044.jpg')">
-    <h1>
-      <?php
-      echo $title;
-      ?>
-    </h1>
-    <p>Your sanctuary for exceptional flavors</p>
-    <nav>
-      <a href="<?php echo $src ?>">Our mission</a>
-    </nav>
-  </header>
-
-  <main>
+<?php include 'inc/header.inc.php'; ?>
     <h1>Our mission</h1>
     <p>
       At Culinary Cove, our core mission is to offer a sanctuary for those who
@@ -39,10 +9,7 @@
       striving to create unforgettable meals that resonate with our guests.
     </p>
     <figure style="text-align: center">
-      <img
-        style="max-width: 20rem"
-        src="images/pexels-burak-the-weekender-735869.jpg"
-        alt="Our amazing restaurant" />
+      <img style="max-width: 20rem" src="images/pexels-burak-the-weekender-735869.jpg" alt="Our amazing restaurant" />
       <figcaption>Our amazing restaurant</figcaption>
     </figure>
     <p>
@@ -55,11 +22,4 @@
       than just a dish; it's a journey into a world of unique and exciting
       flavors.
     </p>
-  </main>
-
-  <footer>
-    <p>Culinary Cove's website.</p>
-  </footer>
-</body>
-
-</html>
+  <?php include 'inc/footer.inc.php'; ?>
